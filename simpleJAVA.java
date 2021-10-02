8b50628d4fbd35147f03a1ca470b91f54acd289d
Shenu98 <91783648+Shenu98@users.noreply.github.com>
2021-10-02 17:32:36 +0530

    Create simpleJAVA.java

---
+++ simpleJAVA.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(     );
+
+    System.out.println("Hello Welcome To HacktoberFesr!\n");
+
+    System.out.print("Enter Your Name : ");
+    String age = name.next(); //scan name
+
+    System.out.print("Enter Your Country : "); 
+    String country = scan.next(); //scan country
+
+    System.out.print("\n");
+    System.out.println("Thank You " + name); //print name
+    System.out.println("~" +naem+ "~"); //print country
+    System.out.print("\n");
+
+  }
+}
